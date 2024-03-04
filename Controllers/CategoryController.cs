349cb2242d1bb2341b09d13a683b115e352c03b9
Thiago Barbieri Catarina <thiago.barbieri2002@gmail.com>
2024-03-04 15:40:12 -0300

    Get Categories from the DB

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookStore.Data;
+using BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookStore.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db; 
+        }
         public IActionResult Index()
         {
+            List<Category> objCategoryList = _db.Categories.ToList();
             return View();
         }
     }
