92483b3bef2a4c208d8b613f883622084e26f481
Thiago Barbieri Catarina <thiago.barbieri2002@gmail.com>
2024-03-04 20:48:09 -0300

    Add data annotation

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,13 +1,16 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BookStore.Models
 {
     public class Category
     {
-        [Key]        
+        [Key]
         public int Id { get; set; }
         [Required]
+        [DisplayName("Category Name")]
         public string Name { get; set; }
+        [DisplayName("Display Name")]
         public int DisplayOrder { get; set; }
     }
 }
