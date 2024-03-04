f83796346feccd1cdfa5c23874d8d13518588c84
Thiago Barbieri Catarina <thiago.barbieri2002@gmail.com>
2024-03-04 16:09:20 -0300

    Return Category list to the View model

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -14,7 +14,7 @@ namespace BookStore.Controllers
         public IActionResult Index()
         {
             List<Category> objCategoryList = _db.Categories.ToList();
-            return View();
+            return View(objCategoryList);
         }
     }
 }
