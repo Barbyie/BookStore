938f3cb4d16f31d2ff60e619c2e581611cbdc118
Thiago Barbieri Catarina <thiago.barbieri2002@gmail.com>
2024-03-04 20:33:35 -0300

    Add Create Category action

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -16,5 +16,9 @@ namespace BookStore.Controllers
             List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
+        public IActionResult Create()
+        {
+            return View();  
+        }
     }
 }
