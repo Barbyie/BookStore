9ed02691a61af954683b8c639bb2e44df6702ca6
Thiago Barbieri Catarina <thiago.barbieri2002@gmail.com>
2024-03-05 00:38:36 -0300

    Creat POST action for the Category Page

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -20,5 +20,16 @@ namespace BookStore.Controllers
         {
             return View();  
         }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");  
+            }
+            return View(obj);  
+        }
     }
 }
