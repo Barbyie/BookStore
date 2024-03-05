d01f88ddc2afa741e0070cbd9da786ea8951d959
Thiago Barbieri Catarina <thiago.barbieri2002@gmail.com>
2024-03-05 08:39:16 -0300

    Create Delete Action and Delete POST action

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -56,5 +56,31 @@ namespace BookStore.Controllers
             }
             return View(obj);  
         }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);  
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
